df0d93af218868fac39e3b2523984cac94fae361
Maksimova Evgeniya <Nikitina_jenya1997@mail.ru>
2024-01-13 13:47:49 +0600

    Создан компонент для построения списка пунктов меню

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
-import './components/menu-items/MenuItem'
 import './App.css'
-import foodImage from '../../assets/food.png';
-import hotDrinkImage from '../../assets/hot-drink.png';
+import MenuList from "./components/MenuList/MenuList";
 
-type Item = {
-  name: string;
-  price: number;
-  image: string;
-}
 
-const ITEMS: Item[] = [
-  {name: 'Hamburger', price: 80, image: foodImage},
-  {name: 'Cheeseburger', price: 90, image: foodImage},
-  {name: 'Fries', price: 45, image: foodImage},
-  {name: 'Coffee', price: 70, image: hotDrinkImage},
-  {name: 'Tea', price: 50, image: hotDrinkImage},
-  {name: 'Cola', price: 40, image: hotDrinkImage}
-]
+
 
 function App() {
 
   return (
     <>
+     <MenuList/>
     </>
   )
 }
